f7ecc18ab14ef30b3b1153bd42da5a365b31ad8e
rubensmp <rubens.mp1@gmail.com>
2024-05-27 15:37:37 -0300

    feat: add Exercise model

---
+++ prisma/migrations/20240526191611_add_birthday_to_model_user/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - Added the required column `birthday` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "birthday" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_users" ("created_at", "email", "id", "name", "password") SELECT "created_at", "email", "id", "name", "password" FROM "users";
+DROP TABLE "users";
+ALTER TABLE "new_users" RENAME TO "users";
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+PRAGMA foreign_key_check("users");
+PRAGMA foreign_keys=ON;
