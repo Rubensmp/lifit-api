9cc5acf28202ea40630518b1b5855b1b3acc4ef0
rubensmp <rubens.mp1@gmail.com>
2024-05-29 23:10:37 -0300

    chore: add tsup and create build script

--- src/utils/isAuthenticathed.ts
+++ src/utils/isAuthenticathed.ts
@@ -1,25 +1,28 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { verify } from "../lib/jwt.js";
 import { BadRequest } from "../routes/_errors/bad-request.js";
-import { JwtPayload } from 'jsonwebtoken';
+import { JwtPayload } from "jsonwebtoken";
 
 // Declaração de módulo para estender a interface FastifyRequest
-declare module 'fastify' {
+declare module "fastify" {
   interface FastifyRequest {
     user?: JwtPayload;
   }
 }
 
-export async function isAuthenticated(request: FastifyRequest, reply: FastifyReply) {
+export async function isAuthenticated(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
   const rawToken = request.headers.authorization;
-  const tokenParts = rawToken?.split('Bearer ');
+  const tokenParts = rawToken?.split("Bearer ");
   const accessToken = tokenParts?.[1];
 
-  if (!accessToken) throw new BadRequest('Token not found.');
+  if (!accessToken) throw new BadRequest("Token not found.");
 
   const payload = await verify(accessToken);
 
-  if (!payload) return reply.code(401).send({ error: 'Invalid token' });
+  if (!payload) return reply.code(401).send({ error: "Invalid token" });
 
   request.user = payload;
 }
